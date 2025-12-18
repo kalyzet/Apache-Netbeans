0c3e7431db82472c490c6a453629aa63c670d83b
Muhammad Haikal <muhammadhaikal3037@gmail.com>
2025-12-18 09:04:38 +0800

    Update GaBisaBasaEnggres.java

--- translate-online/GaBisaBasaEnggres.java
+++ translate-online/GaBisaBasaEnggres.java
@@ -5,7 +5,6 @@ package com.haikal.gabisabasaenggres;
 public class GaBisaBasaEnggres {
     private static FrameEnggres frame;
     public static void main(String[] args) {
-        //System.out.println("Jawa Jawa Jawa!");
         frame = new FrameEnggres();
     } 
 }
