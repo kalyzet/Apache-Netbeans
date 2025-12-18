4f1f342c218e2ebdbb0d57214ac1965b2785d3b6
Muhammad Haikal <muhammadhaikal3037@gmail.com>
2025-12-18 08:31:53 +0800

    Add files via upload

---
+++ translate-online/GaBisaBasaEnggres.java
@@ -0,0 +1,11 @@
+
+package com.haikal.gabisabasaenggres;
+
+
+public class GaBisaBasaEnggres {
+    private static FrameEnggres frame;
+    public static void main(String[] args) {
+        //System.out.println("Jawa Jawa Jawa!");
+        frame = new FrameEnggres();
+    } 
+}
